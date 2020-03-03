6e9f6bc0cfdd7f8f94809957269523cfefc4908e
Sameer Puri <sameer@users.noreply.github.com>
2020-03-03 15:33:03 -0500

    Starting to use DBus

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,56 @@
+// import * as browser from 'webextension-polyfill';
+import * as dbus from 'dbus-next';
+
+const MPRIS_IFACE = 'org.mpris.MediaPlayer2.Player';
+const MPRIS_PATH = '/org/mpris/MediaPlayer2';
+const PROPERTIES_IFACE = 'org.freedesktop.DBus.Properties';
+
+const bus = dbus.sessionBus();
+
+let {
+    Interface, property, method, signal,
+    ACCESS_READ, ACCESS_WRITE, ACCESS_READWRITE
+} = dbus.interface;
+
+class MPRISInterface extends Interface {
+    @property({ signature: 's', access: ACCESS_READ })
+    PlaybackStatus: string = 'stopped';
+
+    @method({ inSignature: '', outSignature: '' })
+    Next(): void {
+    }
+
+    @method({ inSignature: '', outSignature: '' })
+    Previous(): void {
+    }
+
+    @method({ inSignature: '', outSignature: '' })
+    Pause(): void {
+    }
+
+    @method({ inSignature: '', outSignature: '' })
+    PlayPause(): void {
+    }
+
+    @method({ inSignature: '', outSignature: '' })
+    Stop(): void {
+    }
+
+    @method({ inSignature: '', outSignature: '' })
+    Play(): void {
+    }
+
+    @method({ inSignature: 'x', outSignature: '' })
+    Seek(offset: number): void {
+    }
+
+    @method({ inSignature: 'ox', outSignature: '' })
+    SetPosition(trackId: string, position: number): void {
+    }
+}
+
 browser.runtime.onMessage.addListener(
-    (message: any, sender: any): boolean => {
+    (message: any, _sender: any): boolean => {
+        bus.export(`${MPRIS_PATH}/${message}`, new MPRISInterface(message));
         return true;
     });
